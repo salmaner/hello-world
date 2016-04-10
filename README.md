eb00ae395e097a8191279c170b6ab30e469c05c1
SergeyRykov <rykcer@mail.ru>
2016-04-10 19:47:58 +0600

    комичу на домаш. manjaro
    добавил файл ту ду, вернулся к хранилищу loclabase
    продолжаю выбирать какой формат данных лучше использовать
    для хранения копии сайта локально.

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # hello-world
-to share idias
-
-hello every one, may be it is so exiting to do something
-together... what could be done? :)
 
 OK, 4 javascript's files are here!
+One public folder whith tow  files.
--- public/planet.js
+++ public/planet.js
@@ -4,26 +4,35 @@ var code = access.outerHTML;
 var planet = document.getElementById("greenplanet");
 console.log("Полное содержание тега" + access.outerHTML);
 
-var sometxt = "<p>примерчик из книги для особо Одаренных. где надо было вводить<br>"+
+var sometxt = "<h3>Blue Planet</h3>"+
+"<p id=\"blueplanet\">All systems A-OK</p>"+
+"<p>примерчик из книги для особо Одаренных. где надо было вводить<br>"+
 "номер телефона, менять цвет используя стили.<br>"+
 "добавил поле инпут и вывожу как тескт. столкнулся с проблемой<br>"+
 "с регулярнымы выражениями. по книге они принимали цифвры с дефисом<br>"+
 "и матчили их. через поле ипут они этого не делают.<br><p>"
 
+// создаем одну запись в для локального хранилища
+// один ключ одна запись
+var myArray = ["One", "Two", ["One", "Two"], "four"];
+var myArray2 = ["кружка","бочка",[100,200,900,800],"ложка"];
+localStorage["myArray1"] = JSON.stringify(myArray);
+localStorage["myArray2"] = JSON.stringify(myArray2);
+console.log(localStorage["myArray"]);
 
+// - - - - declare all funcitons which we need - - - - -
 function validate() {
     var phoneNumber = document.getElementById("inner").value; 
     return phoneNumber;
-    // нахуй эти регулярные выражения, то работают, то нет.
+    // наkуй эти регулярные выражения, то работают, то нет.
     // return phoneNumber.match(/^\d{3}-?\d{4}$/); // регулярные выражения ...
 }
 
 
 function Bstart() {
     console.log(planet);
-    planet.innerHTML = "Red Alert: поменяли цвет и выражениеы!";
+    planet.innerHTML = "Red Alert: поменяли цвет и Тип с его значением!";
     planet.setAttribute("class", "redtext");
-    
     
     x = validate();
     var tipo = document.getElementById("PoutTxt");
@@ -39,25 +48,20 @@ function Bshotxt () {
     tipo.innerHTML = sometxt;
 }
 
+function Bshwoloc () {
+    console.log("записанно в хранилища: "+localStorage.length);
+    for (var i = 0; i < localStorage.length; i++) {
+       key = localStorage.key(i);
+       data = JSON.parse(localStorage[key]);
+       console.log(data);
+    }
+}
 
 function init() {
     document.getElementById("bstart").onclick = Bstart;
     document.getElementById("bclear").onclick = Bclear;
     document.getElementById("bwhotxt").onclick = Bshotxt;
-
-
-    
-    
+    document.getElementById("bsholoc").onclick = Bshwoloc;
 }
 
 window.onload = init;
-
-// ===================================== был какой то примерчик
-// function Duck(sound) {
-//     this.sound = sound;
-//     this.quack = function() { console.log(this.sound); }
-// }
-// var toy = new Duck("gav gav gav");
-// toy.quack();
-// console.log(typeof toy);
-// console.log(toy instanceof Duck);
